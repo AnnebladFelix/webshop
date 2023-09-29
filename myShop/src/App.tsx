4de38172be66b27f552f7a2d3afeba0e9ddb8560
Felix Anneblad <felix.anneblad@gmail.com>
2023-09-29 10:58:28 +0200

    Added sample products

--- myShop/src/App.tsx
+++ myShop/src/App.tsx
@@ -1,11 +1,30 @@
 
 import './App.css'
+import { sampleProducts } from './Data'
 
 function App() {
   return (
     <>
-      <header>My webshop</header>
-      <main></main>
+      <header><h1>My webshop</h1></header>
+      <main>
+        {sampleProducts.map(product => (
+          <li key={product.slug}>
+            <h2>
+              {product.name}
+            </h2>
+            <p>
+              {product.category}
+            </p>
+            <p>
+              {product.brand}
+            </p>
+            <p>
+              {product.price} kr
+            </p>
+            <img className='product-image' src={product.image} alt={product.name} />
+          </li>
+        )) }
+      </main>
       <footer></footer>
     </>
   )
