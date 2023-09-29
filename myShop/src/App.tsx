a1a6eaea2f9dbb1e688a0b1ace4a95fb484f45ad
Felix Anneblad <felix.anneblad@gmail.com>
2023-09-29 11:32:11 +0200

    Added bootstrap to project

--- myShop/src/App.tsx
+++ myShop/src/App.tsx
@@ -1,31 +1,51 @@
-
-import './App.css'
 import { sampleProducts } from './Data'
+import { Col, Container, Nav, Navbar, Row } from 'react-bootstrap'
 
 function App() {
   return (
     <>
-      <header><h1>My webshop</h1></header>
-      <main>
-        {sampleProducts.map(product => (
-          <li key={product.slug}>
-            <h2>
-              {product.name}
-            </h2>
-            <p>
-              {product.category}
-            </p>
-            <p>
-              {product.brand}
-            </p>
-            <p>
-              {product.price} kr
-            </p>
-            <img className='product-image' src={product.image} alt={product.name} />
-          </li>
-        )) }
-      </main>
-      <footer></footer>
+      <div className='d-flex flex-column' id='app-container'>
+        <header>
+          <Navbar bg='dark' variant='dark' expand='lg' >
+            <Container>
+              <Navbar.Brand href='#home'>
+                My Shop
+              </Navbar.Brand>
+            </Container>
+              <Navbar.Toggle aria-controls='basic-navbar-nav' />
+              <Navbar.Collapse id='basic-navbar-nav'>
+                <Nav className='me-auto'>
+                  <Nav.Link href='#home'>Home</Nav.Link>
+                  <Nav.Link href='#link'>Link</Nav.Link>
+                </Nav>
+              </Navbar.Collapse>
+          </Navbar>
+        </header>
+        <main>
+          <Container className='mt-3'>
+            <Row>
+              {sampleProducts.map(product => (
+                <Col key={product.slug} sm={6} md={4} lg={3}>
+                  <h2>
+                    {product.name}
+                  </h2>
+                  <p>
+                    {product.category}
+                  </p>
+                  <p>
+                    {product.brand}
+                  </p>
+                  <p>
+                    {product.price} kr
+                  </p>
+                  <img className='product-image' src={product.image} alt={product.name} />
+                </Col>
+              )) }
+            </Row>
+          </Container>
+        </main>
+        <footer></footer>
+      </div>
     </>
   )
 }
